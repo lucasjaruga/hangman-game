2501426671a9842c3bc9607a53f39674202b519d
Lucas Jaruga <lucas.jaruga@outlook.com>
2021-02-21 22:04:01 +0100

    implemented logic for playing the game

--- src/pl/jarugalucas/Game.java
+++ src/pl/jarugalucas/Game.java
@@ -3,17 +3,22 @@ package pl.jarugalucas;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
- * Game logic here
+ * Game class - responsible for a game logic
  */
 public class Game {
 
-    private final Integer noTries = 7;
-    private ArrayList<String> dictionary;
+    private final int maxTries = 7;
+    private int userTries = 0;
+    private final ArrayList<String> dictionary;
+    private String wordToGuess;
+    private StringBuilder secretWord;
+    public ArrayList<Character> userGuesses;
 
     public Game(){
+        userGuesses = new ArrayList<>();
         dictionary = new ArrayList<>();
         try {
             BufferedReader bufferedReader =
@@ -23,24 +28,162 @@ public class Game {
                 dictionary.add(word);
             }
         } catch (IOException e) {
-            System.out.println("Couldn't find a dictionary file!");
+            System.out.println("Couldn't find a dictionary file! Sorry, we won't play :(");
+        }
+    }
+
+    // choose word to guess and prepare secretWord
+    public void wordToGuess(){
+        Random random = new Random();
+        int wordIndex = random.nextInt(dictionary.size());
+        wordToGuess = dictionary.get(wordIndex);
+
+        int wordLength = wordToGuess.length();
+        secretWord = new StringBuilder();
+        for(int i = 0; i < wordLength * 2; i++){
+            if(i%2 == 0){
+                secretWord.append("_");
+            } else {
+                secretWord.append(" ");
+            }
+        }
+    }
+
+    public String getSecretWord(){
+        return secretWord.toString();
+    }
+
+    public void setUserTries(Integer userTries) {
+        this.userTries = userTries;
+    }
+
+    public boolean isGameOver(){
+        return userTries == maxTries;
+    }
+
+    public boolean checkUserGuess(char userCharGuess){
+        // check if user already used that character, if not add to list
+        boolean alreadyCheck = false;
+        Iterator iterator = userGuesses.iterator();
+        while(iterator.hasNext()) {
+            if (iterator.next().equals(userCharGuess)) {
+                System.out.println("You've already tried this one. Try another one :)");
+                System.out.println(secretWord);
+                alreadyCheck = true;
+                break;
+            }
         }
 
+        //if not already check then add to a list
+        if(!alreadyCheck){
+            userGuesses.add(userCharGuess);
+        }
 
-    };
+        return alreadyCheck;
+    }
 
-    /*
-    Picture to use
+    public boolean checkIfItIsInTheWord(char userCharGuess){
+        boolean foundAny = false;
+        for(int i = 0; i < wordToGuess.length(); i++){
+            if(wordToGuess.charAt(i) == userCharGuess){
+                secretWord.setCharAt(i * 2, userCharGuess);
+                foundAny = true;
+            }
+        }
 
-    "  - - - - -\n"+
-    " |        |\n"+
-    " |        O\n" +
-    " |      / | \\ \n"+
-    " |        |\n" +
-    " |       / \\ \n" +
-    " |\n" +
-    "/ \\n";
+        if(foundAny){
+            return true;
+        } else {
+            userTries++;
+            return false;
+        }
+    }
 
-    */
+    /**
+     * method responsible for drawing picture
+     *
+     * @return an appropriate picture of hangman based on used tries by player
+     */
+    public String drawPicture(){
+        /*
+            Picture to use
 
+            "  - - - - -\n"+
+            " |        |\n"+
+            " |        O\n" +
+            " |      / | \\ \n"+
+            " |        |\n" +
+            " |       / \\ \n" +
+            " |\n" +
+            "/ \\n";
+         */
+
+        switch (userTries){
+            case 1: return  "  - - - - -\n"+
+                            " |        |\n"+
+                            " |        O\n" +
+                            " |       \n"+
+                            " |        \n" +
+                            " |       \n" +
+                            " |\n" +
+                            "/ \\";
+
+            case 2: return  "  - - - - -\n"+
+                    " |        |\n"+
+                    " |        O\n" +
+                    " |        | \n"+
+                    " |        \n" +
+                    " |        \n" +
+                    " |\n" +
+                    "/ \\";
+            case 3: return  "  - - - - -\n"+
+                    " |        |\n"+
+                    " |        O\n" +
+                    " |        |\\  \n"+
+                    " |        \n" +
+                    " |        \n" +
+                    " |\n" +
+                    "/ \\";
+            case 4: return  "  - - - - -\n"+
+                    " |        |\n"+
+                    " |        O\n" +
+                    " |      / | \\ \n"+
+                    " |        \n" +
+                    " |         \n" +
+                    " |\n" +
+                    "/ \\";
+            case 5: return  "  - - - - -\n"+
+                    " |        |\n"+
+                    " |        O\n" +
+                    " |      / | \\ \n"+
+                    " |        |\n" +
+                    " |        \n" +
+                    " |\n" +
+                    "/ \\";
+            case 6: return  "  - - - - -\n"+
+                    " |        |\n"+
+                    " |        O\n" +
+                    " |      / | \\ \n"+
+                    " |        |\n" +
+                    " |       / \n" +
+                    " |\n" +
+                    "/ \\";
+            case 7: return  "  - - - - -\n"+
+                    " |        |\n"+
+                    " |        O\n" +
+                    " |      / | \\ \n"+
+                    " |        |\n" +
+                    " |       / \\ \n" +
+                    " |\n" +
+                    "/ \\";
+            default: return  "  - - - - -\n"+
+                    " |        |\n"+
+                    " |         \n" +
+                    " |         \n"+
+                    " |         \n" +
+                    " |         \n" +
+                    " |         \n" +
+                    "/ \\ ";
+        }
+    }
 }
--- src/pl/jarugalucas/HangmanApp.java
+++ src/pl/jarugalucas/HangmanApp.java
@@ -27,16 +27,54 @@ public class HangmanApp {
                 incorrectInput = false;
 
         } while(incorrectInput);
-        userInput.close();
+
 
         // start a game if input == 'Y' ELSE terminate the program
-        Game game;
-        if(charToCheck == 'y'){
-            game = new Game();
-        } else {
+        if(charToCheck == 'n'){
+            userInput.close();
             System.exit(-1);
         }
 
+        Game game = new Game();
+
+        // TODO implement preparing the game
+        System.out.println("Superb! :D Let's play!");
+        game.wordToGuess();
+        System.out.println(game.drawPicture());
+        System.out.println("\nYour current guess: " +
+                "" + game.getSecretWord());
+
+
+        // TODO implement playing the game
+        // ask user for a guess
+        // check if user already checked that letter
+            // if not check if it is in the word
+                //
+
+            // if yes ask user again about letter
+
+        while(!game.isGameOver()){
+            System.out.print("Guess a character: ");
+            charToCheck = userInput.nextLine().toLowerCase().charAt(0);
+            System.out.println();
+            boolean tried = game.checkUserGuess(charToCheck);
+
+            if(!tried){
+                boolean foundAny = game.checkIfItIsInTheWord(charToCheck);
+                if(foundAny){
+                    System.out.println("Great guess! :D \n");
+                    System.out.println(game.getSecretWord());
+                } else {
+                    System.out.println("Ups! There is no letter like this one :(\n");
+                    System.out.println(game.getSecretWord());
+                }
+                System.out.println(game.drawPicture());
+            }
+        }
+
         // TODO add code to check if user want to play again
+
+
+        userInput.close();
     }
 }
